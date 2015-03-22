3288a48114b4fdaa21ab9d079db1ce4f57c7c94a
live4code <live4code@gmail.com>
2015-03-22 08:50:47 +0000

    add function map

--- function-map/function-map.js
+++ function-map/function-map.js
@@ -1,7 +1,7 @@
 // implement the doubleAll function using function map
 module.exports.doubleAll = function (numbers) {
   return numbers.map(function double(num) {
-    
+    return num*2;
     
   })
 }
