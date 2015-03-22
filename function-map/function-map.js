305a2809a556fe9bcf189be40cbdc81db6b650c5
live4code <live4code@gmail.com>
2015-03-22 08:48:32 +0000

    commit exercise content by Live4Code

--- function-map/function-map.js
+++ function-map/function-map.js
@@ -1,6 +1,6 @@
 // implement the doubleAll function using function map
 module.exports.doubleAll = function (numbers) {
   return numbers.map(function double(num) {
-    
+    return num * 2;
   })
 }
